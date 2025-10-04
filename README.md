5adafd5750aae93add0bdbea9a5da21211f10779
hrz <muhammad9harraz@gmail.com>
2025-10-04 16:24:00 +0800

    Changes to line spacing

--- README.md
+++ README.md
@@ -17,8 +17,11 @@ A simple command-line calculator built with Python.
 4. Divide
 5. Exit
 Enter a choice: 1
+
 Enter first number: 13
+
 Enter second number: 7
+
 Result:  20.0
 
 -----Calculator-----
@@ -27,10 +30,32 @@ Result:  20.0
 3. Multiply
 4. Divide
 5. Exit
+Enter a choice: 2
+
+Enter first number: 31
+
+Enter second number: e
+
+Invalid input! Please enter a number.
+
+Enter second number: 1
+
+Result:  30.0
+
+-----Calculator-----
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+5. Exit
 Enter a choice: 4
-Enter first number: 20
+
+Enter first number: 44
+
 Enter second number: 0
-Result:  Error: Division by zero!
+
+Result:
+Error: Division by zero!
 
 -----Calculator-----
 1. Add
@@ -39,9 +64,12 @@ Result:  Error: Division by zero!
 4. Divide
 5. Exit
 Enter a choice: 4
-Enter first number: 20
+
+Enter first number: 44
+
 Enter second number: 4
-Result:  5.0
+
+Result:  11.0
 
 -----Calculator-----
 1. Add
@@ -49,7 +77,8 @@ Result:  5.0
 3. Multiply
 4. Divide
 5. Exit
-Enter a choice: 8
+Enter a choice: 9
+
 Invalid input!
 
 -----Calculator-----
@@ -59,4 +88,5 @@ Invalid input!
 4. Divide
 5. Exit
 Enter a choice: 5
+
 Exiting calculator...
--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ def multiply(x, y):
 
 def divide(x, y):
     if y == 0:
-        return "Error: Division by zero!"
+        return "\nError: Division by zero!"
     return x / y
 
 def get_input(prompt):
@@ -17,10 +17,10 @@ def get_input(prompt):
         try:
             return float(input(prompt))
         except ValueError:
-            print("Invalid input! Please enter a number.")
+            print("\nInvalid input! Please enter a number.")
 
 while True:
-    print("-----Calculator-----")
+    print("\n-----Calculator-----")
     print("1. Add")
     print("2. Subtract")
     print("3. Multiply")
@@ -32,20 +32,20 @@ while True:
     if choice in ["1", "2", "3", "4", "5"]:
 
         if choice == "5":
-            print("Exiting calculator...")
+            print("\nExiting calculator...")
             break
 
-        num1 = get_input("Enter first number: ")
-        num2 = get_input("Enter second number: ")
+        num1 = get_input("\nEnter first number: ")
+        num2 = get_input("\nEnter second number: ")
 
         if choice == "1":
-            print("Result: ", add(num1, num2))
+            print("\nResult: ", add(num1, num2))
         elif choice == "2":
-            print("Result: ", subtract(num1, num2))
+            print("\nResult: ", subtract(num1, num2))
         elif choice == "3":
-            print("Result: ", multiply(num1, num2))
+            print("\nResult: ", multiply(num1, num2))
         elif choice == "4":
-            print("Result: ", divide(num1, num2))
+            print("\nResult: ", divide(num1, num2))
         
     else:
-        print("Invalid input!")
+        print("\nInvalid input!")
